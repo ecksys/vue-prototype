8c041e1196029ff77d96209e6c6e7145b7a508cd
ecksys <xuthiensun@gmail.com>
2019-02-24 21:50:50 +0800

    Add eventBus for basic attack events

--- src/components/Monster.vue
+++ src/components/Monster.vue
@@ -2,12 +2,29 @@
     <div class="col">
         <h2>Monster</h2>
         <div class="progress">
-            <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+            <div class="progress-bar bg-success" :style="{ width: health + '%' }">{{ health }}</div>
         </div>
+        {{ test }}
     </div>
 </template>
 
 <script>
+    import { eventBus } from '../main';
+
+    export default {
+        data() {
+            return {
+                health: 100,
+                attack: 2
+            }
+        },
+        created() {
+            eventBus.$on('playerHasAttacked', (damage) => {
+                this.health -= damage;
+                eventBus.$emit('monsterHasAttacked', this.attack);
+            });
+        }
+    }
 </script>
 
 <style lang="scss">
--- src/components/Player.vue
+++ src/components/Player.vue
@@ -2,14 +2,35 @@
     <div class="col">
         <h2>Player</h2>
         <div class="progress">
-            <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+            <div class="progress-bar bg-success" :style="{ width: health + '%' }">{{ health }}</div>
         </div>
-        <button>Attack</button>
+        <button @click="playerAttack">Attack</button>
         <button>Special</button>
+        <p>Current Attack: {{ attack }}</p>
     </div>
 </template>
 
 <script>
+    import { eventBus } from '../main';
+
+    export default {
+        data() {
+            return {
+                health: 100,
+                attack: 1
+            }
+        },
+        methods: {
+            playerAttack() {
+                eventBus.$emit('playerHasAttacked', this.attack);
+            }
+        },
+        created() {
+            eventBus.$on('monsterHasAttacked', (damage) => {
+                this.health -= damage;
+            })
+        }
+    }
 </script>
 
 <style lang="scss">
